d9e2d1a31475dd551f7f6ab11eac68fa472c146b
Arnav Raj <arnavrajcodes@gmail.com>
2025-06-25 17:41:47 +0530

    tailwind

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,15 +1,5 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
-  theme:{
-    extend:{
-      colors:{
-        black:{
-          DEFAULT:'#000',
-          100:'#000319'
-        }
-      }
-    }
-  }
 };
 
 export default config;
